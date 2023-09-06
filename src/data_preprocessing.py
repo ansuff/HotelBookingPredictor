f8a56e68a93ba9e9c3a1412ba293c7861ec41866
Anas Suffo <anassaffo@gmail.com>
2023-09-06 11:10:58 +0200

    Adding more EDA

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,60 +1,195 @@
 import pandas as pd
 from tqdm import tqdm
-class DataPreprocessor:
-    def __init__(self, data):
-        self.data = data
+from sklearn.preprocessing import OrdinalEncoder
 
-    def convert_datetime(self):
+class DataPreprocessor:
+    '''
+    This class contains the methods for preprocessing the data.
+    
+    The methods are:
+    - convert_datetime
+    - remove_cancelled_bookings
+    - drop_na_columns
+    - drop_duplicates
+    - encode_categorical_variables
+    
+    The fit_transform method applies all the preprocessing steps to the data.
+    You can eaiter use the fit_transform method or apply the methods one by one.
+    '''
+    def __init__(self):
+        pass
+
+    def convert_date_columns_to_datetime(self, data):
+        '''
+        This method converts the date columns to datetime.
+        
+        The date columns are:
+        - arrival_date
+        - departure_date
+        - booking_date
+        '''
         # convert date columns to datetime
         print('Converting date columns to datetime...')
         with tqdm(total=3) as pbar:
-            self.data['arrival_date'] = pd.to_datetime(self.data['arrival_date'])
+            data = data.copy()
+            data['arrival_date'] = pd.to_datetime(data['arrival_date'])
             pbar.update(1)
-            self.data['departure_date'] = pd.to_datetime(self.data['departure_date'])
+            data['departure_date'] = pd.to_datetime(data['departure_date'])
             pbar.update(1)
-            self.data['booking_date'] = pd.to_datetime(self.data['booking_date'])
+            data['booking_date'] = pd.to_datetime(data['booking_date'])
             pbar.update(1)
-        print('Date columns converted to datetime.')
-
-    def drop_na_columns(self):
-        # drop rows with missing values
-        print('Dropping rows with missing values...')
+        print('Date columns converted to datetime.\n')
+        return data
+        '''   
+    def remove_cancelled_bookings(self, data):
+        '''
+        #This method removes cancelled bookings.
+        
+        #The data contains bookings that were cancelled.
+        #These bookings have the value 1 in the column 'is_canceled'.
+        '''
+        # remove cancelled bookings
+        print('Removing cancelled bookings...')
+        number_of_rows_before = data.shape[0]
         with tqdm(total=1) as pbar:
-            self.data.dropna(inplace=True)
-            pbar.update(1)
-        print('Rows with missing values dropped.')
-
-    def drop_duplicates(self):
-        # drop duplicate rows
-        print('Dropping duplicate rows...')
-        number_of_rows_before = self.data.shape[0]
-        with tqdm(total=1) as pbar:
-            self.data.drop_duplicates(inplace=True)
+            data = data.copy()
+            data = data[data['is_canceled'] == 0]
             pbar.update(1)
         # print how many rows were dropped
-        print(f'{abs(self.data.shape[0]- number_of_rows_before)} rows dropped.')
+        print(f'{abs(data.shape[0]- number_of_rows_before)} rows dropped from {number_of_rows_before}.\n')
+        return data
+        '''
+    def drop_na_rows(self, data):
+        '''
+        This method drops rows with missing values.
+        '''
+        # drop rows with missing values
+        print('Dropping rows with missing values...')
+        number_of_rows_before = data.shape[0]
+        with tqdm(total=1) as pbar:
+            data = data.copy()
+            data.dropna(inplace=True, axis=0)
+            pbar.update(1)
+        print(f'{abs(data.shape[0]- number_of_rows_before)} rows dropped from {number_of_rows_before}.\n')
+        return data
 
-    def encode_categorical_variables(self):
+    def drop_duplicates(self, data):
+        '''
+        This method drops duplicate rows.
+        '''
+        # drop duplicate rows
+        print('Dropping duplicate rows...')
+        number_of_rows_before = data.shape[0]
+        with tqdm(total=1) as pbar:
+            data = data.copy()
+            data.drop_duplicates(inplace=True)
+            pbar.update(1)
+        # print how many rows were dropped
+        print(f'{abs(data.shape[0]- number_of_rows_before)} rows dropped from {number_of_rows_before}.\n')
+        return data
+    
+    def drop_negative_adr(self, data):
+        '''
+        This method drops rows with negative adr.
+        '''
+        # drop rows with negative adr
+        print('Dropping rows with negative adr...')
+        number_of_rows_before = data.shape[0]
+        with tqdm(total=1) as pbar:
+            data = data.copy()
+            data = data[data['adr'] >= 0]
+            pbar.update(1)
+        # print how many rows were dropped
+        print(f'{abs(data.shape[0]- number_of_rows_before)} rows dropped from {number_of_rows_before}.\n')
+        return data
+'''
+    def encode_categorical_variables(self, data):
+        '''
+        #This method encodes the categorical variables using the category codes.
+        #The encoding is printed for each column.
+        #The original values are printed for each column.
+        '''
         # encode categorical variables
         print('Encoding categorical variables...')
         with tqdm(total=10) as pbar:
+            data = data.copy()
             for col in ['hotel_name', 'meal', 'source_country', 'market_segment',
                         'distribution_channel', 'assigned_room_type', 'guest_type',
                         'customer_type', 'season', 'company']:
                 # preserve the original values in a new column with the suffix '_original'
-                self.data[col + '_original'] = self.data[col]
-                self.data[col] = self.data[col].astype('category').cat.codes
+                data[col + '_original'] = data[col]
+                data[col] = data[col].astype('category').cat.codes
                 pbar.update(1)
-            print('Categorical variables encoded.')
+            print('Categorical variables encoded.\n')
 
         # print the encoding and the original values for each column
         for col in ['hotel_name', 'meal', 'source_country', 'market_segment',
                     'distribution_channel', 'assigned_room_type', 'guest_type',
                     'customer_type', 'season', 'company']:
             print(f'{col} encoding:')
-            encoding_dict = dict(enumerate(self.data[col].astype('category').cat.categories))
-            original_values = dict(zip(encoding_dict.values(), self.data[col + '_original'].unique()))
+            encoding_dict = dict(enumerate(data[col].astype('category').cat.categories))
+            original_values = dict(zip(encoding_dict.values(), data[col + '_original'].unique()))
             print(f'Original values: {original_values}')
             # remove the original values from the data
-            self.data.drop(columns=[col + '_original'], inplace=True)
-            
+            data.drop(columns=[col + '_original'], inplace=True)
+        print('\n')
+        return data
+  '''  
+    def encode_categorical_variables(self, data):
+        '''
+        This method encodes the categorical variables using ordinal encoding.
+        The encoding is printed for each column.
+        The original values are printed for each column.
+        '''
+        # encode categorical variables
+        print('Encoding categorical variables...')
+        with tqdm(total=10) as pbar:
+            data = data.copy()
+            for col in ['hotel_name', 'meal', 'source_country', 'market_segment',
+                        'distribution_channel', 'assigned_room_type', 'guest_type',
+                        'customer_type', 'season', 'company']:
+                # preserve the original values in a new column with the suffix '_original'
+                data[col + '_original'] = data[col]
+                # use ordinal encoding to encode the categorical variable
+                encoder = OrdinalEncoder()
+                data[col] = encoder.fit_transform(data[[col]])
+                pbar.update(1)
+            print('Categorical variables encoded.\n')
+
+        # print the encoding and the original values for each column
+        for col in ['hotel_name', 'meal', 'source_country', 'market_segment',
+                    'distribution_channel', 'assigned_room_type', 'guest_type',
+                    'customer_type', 'season', 'company']:
+            print(f'{col} encoding:')
+            encoding_dict = dict(enumerate(encoder.categories_[0]))
+            original_values = dict(zip(encoding_dict.values(), data[col + '_original'].unique()))
+            print(f'Original values: {original_values}')
+            # remove the original values from the data
+            data.drop(columns=[col + '_original'], inplace=True)
+        print('\n')
+        return data
+
+    def fit_transform(self, data):
+        '''
+        This method applies all the preprocessing steps to the data.
+        '''
+        data = self.convert_date_columns_to_datetime(data)
+        #data = self.remove_cancelled_bookings(data)
+        data = self.drop_na_rows(data)
+        data = self.drop_duplicates(data)
+        data = self.drop_negative_adr(data)
+        data = self.encode_categorical_variables(data)
+        return data
+    
+    def fit_transform_without_encoding_variables(self,data):
+        '''
+        This method applies all the preprocessing steps to the data.
+        Except for the encoding of the categorical variables.
+        '''
+        data = self.convert_date_columns_to_datetime(data)
+        #data = self.remove_cancelled_bookings(data)
+        data = self.drop_na_rows(data)
+        data = self.drop_duplicates(data)
+        data = self.drop_negative_adr(data)
+        return data
+    
--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,16 +1,86 @@
 import pandas as pd
 
 class HotelBookingFeatures:
-    def __init__(self, data):
-        self.data = data
+    '''
+    This class contains the methods for creating new features from the data.
     
-    def is_weekend_stay(self):
-        arrival_dayofweek = pd.DatetimeIndex(self.data['arrival_date']).dayofweek
-        departure_dayofweek = pd.DatetimeIndex(self.data['departure_date']).dayofweek
-        self.data['is_weekend_stay'] = ((arrival_dayofweek >= 5) | (departure_dayofweek >= 5)).astype(int)
+    The methods are:
+    - is_weekend_stay
+    - num_days_stayed
+    - num_weekend_nights
+    - num_week_nights
+    - booking_lead_time
+    - arrival_date_features
+    
+    The fit_transform method applies all the feature engineering steps to the data.
+    You can eaiter use the fit_transform method or apply the methods one by one.
+    '''
+    def __init__(self):
+        pass
+    
+    def is_weekend_stay(self, data):
+        '''
+        This method creates a new variable 'is_weekend_stay' from the arrival and departure dates.
+        
+        The value of 'is_weekend_stay' is 1 if the stay includes a weekend night.
+        '''
+        arrival_dayofweek = pd.DatetimeIndex(data['arrival_date']).dayofweek
+        departure_dayofweek = pd.DatetimeIndex(data['departure_date']).dayofweek
+        data['is_weekend_stay'] = ((arrival_dayofweek >= 5) | (departure_dayofweek >= 5)).astype(int)
+        return data
             
-    def num_days_stayed(self):
-        self.data['num_days_stayed'] = (pd.DatetimeIndex(self.data['departure_date']) - pd.DatetimeIndex(self.data['arrival_date'])).days
+    def num_days_stayed(self, data):
+        '''
+        This method creates a new variable 'num_days_stayed' from the arrival and departure dates.
+        '''
+        data['num_days_stayed'] = (pd.DatetimeIndex(data['departure_date']) - pd.DatetimeIndex(data['arrival_date'])).days
+        return data
+        '''
+    def num_weekend_and_week_nights(self, data):
+        '''
+        #This method creates new variables 'num_weekend_nights' and 'num_week_nights' from the arrival and departure dates.
+        '''
+        # calculate the duration of the stay in days
+        data['stay_duration'] = (data['departure_date'] - data['arrival_date']).dt.days
+
+        # calculate the number of weekday and weekend days
+        weekday_mask = (data['arrival_date'].dt.weekday < 5) & (data['departure_date'].dt.weekday < 5)
+        weekend_mask = (data['arrival_date'].dt.weekday >= 5) | (data['departure_date'].dt.weekday >= 5)
+        data['num_weekday_days'] = weekday_mask * data['stay_duration']
+        data['num_weekend_days'] = weekend_mask * data['stay_duration']
+
+        # drop the stay duration column
+        data = data.drop('stay_duration', axis=1)
+
+        return data
+        '''          
+    def booking_lead_time(self, data):
+        '''
+        This method creates a new variable 'booking_lead_time' from the arrival and booking dates.
+        '''
+        data['booking_lead_time'] = (pd.DatetimeIndex(data['arrival_date']) - pd.DatetimeIndex(data['booking_date'])).days
+        return data
+
+    # create from the arrival data: day of week, month, year, week of year
+    def arrival_date_features(self, data):
+        '''
+        This method creates new variables from the arrival date.
+        '''
+        data['arrival_dayofweek'] = pd.DatetimeIndex(data['arrival_date']).dayofweek
+        data['arrival_month'] = pd.DatetimeIndex(data['arrival_date']).month
+        data['arrival_year'] = pd.DatetimeIndex(data['arrival_date']).year
+        data['arrival_weekofyear'] = pd.DatetimeIndex(data['arrival_date']).strftime('%W').astype(int)
+        data['arrival_date_month'] = data['arrival_date'].dt.strftime('%b')
+        #data['arrival_weekofmonth'] = data['arrival_date'].dt.strftime('%V')
+        return data
     
-    def booking_lead_time(self):
-        self.data['booking_lead_time'] = (pd.DatetimeIndex(self.data['arrival_date']) - pd.DatetimeIndex(self.data['booking_date'])).days
+    def fit_transform(self, data):
+        '''
+        This method applies all the feature engineering steps to the data.
+        '''
+        data = self.is_weekend_stay(data)
+        data = self.num_days_stayed(data)
+        #data = self.num_weekend_and_week_nights(data)
+        data = self.booking_lead_time(data)
+        data = self.arrival_date_features(data)
+        return data
